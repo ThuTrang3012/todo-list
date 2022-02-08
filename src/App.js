10d2378202a482c53f48c8627eb04e7cb99507f2
ThuTrang3012 <trangdoan121@gmail.com>
2022-02-08 15:36:30 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ToDoInput from "./components/ToDoInput";
+import ToDoList from "./components/ToDoList";
+class App extends Component {
+    state = {
+        items: [],
+        id: uuidv4(),
+        item: "",
+        editItem: false
+    };
+    handleChange = e => {
+        this.setState({
+            item: e.target.value
+        });
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        const newItem = {
+            id: this.state.id,
+            title: this.state.item
+        };
+        const updatedItems = [...this.state.items, newItem];
+
+        this.setState({
+            items: updatedItems,
+            item: "",
+            id: uuidv4(),
+            editItem: false
+        });
+    };
+    clearList = () => {
+        this.setState({
+            items: []
+        });
+    };
+    handleDelete = id => {
+        const filteredItems = this.state.items.filter(item => item.id !== id);
+        this.setState({
+            items: filteredItems
+        });
+    };
+    handleEdit = id => {
+        const filteredItems = this.state.items.filter(item => item.id !== id);
+        const selectedItem = this.state.items.find(item => item.id === id);
+        this.setState({
+            items: filteredItems,
+            item: selectedItem.title,
+            id: id,
+            editItem: true
+        });
+    };
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            div className = "row" >
+            <
+            div className = "col-10 mx-auto col-md-8 mt-5" >
+            <
+            h3 className = "text-capitalize text-center" > todo input < /h3>  <
+            ToDoInput item = { this.state.item }
+            handleChange = { this.handleChange }
+            handleSubmit = { this.handleSubmit }
+            editItem = { this.state.editItem }
+            />  <
+            ToDoList items = { this.state.items }
+            clearList = { this.clearList }
+            handleDelete = { this.handleDelete }
+            handleEdit = { this.handleEdit }
+            />  <
+            /div >  <
+            /div>  <
+            /div >
+        );
+    }
+}
+export default App;
